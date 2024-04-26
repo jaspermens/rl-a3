0756e1a7270b699d98b03e2d95b72891737cf2d6
Rutger Rijnenberg <rjrrijnenberg@gmail.com>
2024-04-26 13:50:37 +0200

    Reinforce updates

--- reinforce_policy_net.py
+++ reinforce_policy_net.py
@@ -10,19 +10,22 @@ class PolicyNet(nn.Module):
         self.n_inputs = n_inputs
         self.n_actions = n_actions
 
-        n_nodes = 128
+        n_nodes = 64
         self.layer1 = nn.Linear(n_inputs, n_nodes)
         self.act1 = nn.Tanh()
         self.layer2 = nn.Linear(n_nodes, n_nodes)
         self.act2 = nn.Tanh()
-        self.layer3 = nn.Linear(n_nodes, n_actions)
+        self.layer3 = nn.Linear(n_nodes, n_nodes)
+        self.act3 = nn.Tanh()
+        self.layer4 = nn.Linear(n_nodes, n_actions)
         
     def forward(self, x):
         if isinstance(x, np.ndarray):
             x = torch.Tensor(np.array([x]))
         x = self.act1(self.layer1(x))
         x = self.act2(self.layer2(x))
-        return self.layer3(x)
+        x = self.act3(self.layer3(x))
+        return self.layer4(x)
     
     def get_policy(self, x):
         return Categorical(logits=self.forward(x)) # TODO: props or logits??
