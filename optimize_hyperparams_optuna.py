6e56cb6a554806c900379c4a5e78fcfd44c0563f
jmens <jasper.mens@live.nl>
2024-04-26 14:11:25 +0200

    early stopping fudge factor

--- optimize_hyperparams_optuna.py
+++ optimize_hyperparams_optuna.py
@@ -3,9 +3,9 @@ import numpy as np
 from babymode_actorcritic import LunarLanderAC
 
 def objective(trial) -> float:
-    lr = trial.suggest_float("lr", 1e-4, 1e-2, log=True)
+    lr = trial.suggest_float("lr", 1e-4, 1e-3, log=True)
     gamma = trial.suggest_float("gamma", .9, 1)
-    entropy_reg_factor = trial.suggest_float("eta", 1e-5, 1, log=True)
+    entropy_reg_factor = trial.suggest_float("eta", 1e-5, 1e-2, log=True)
     backup_depth = trial.suggest_int("backup_depth", 10, 200, step=5)
     
     model_params = {
@@ -24,13 +24,14 @@ def objective(trial) -> float:
         actorcritic = LunarLanderAC(**model_params)
         actorcritic.train_model()
     
-        finalscores.append(actorcritic.eval_returns[-1])
+        finalscores.append(actorcritic.eval_returns[-1] - actorcritic.total_time/model_params['num_training_steps'] * 100)
 
     return np.mean(finalscores)
 
 def do_study():
     study = optuna.create_study(direction="maximize", 
-                                sampler=optuna.samplers.GPSampler)
+                                sampler=optuna.samplers.GPSampler())
+    
     study.optimize(objective, n_trials=10)
     
     print("best params:", study.best_params)
