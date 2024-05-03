aaf864db46d63bd90d7cfc5aa4f45e333158a50a
jmens <jasper.mens@live.nl>
2024-05-03 15:31:05 +0200

    added toggles for bootstrapping and baseline subtraction

--- model_parameters.py
+++ model_parameters.py
@@ -10,6 +10,11 @@ class ModelParameters:
     early_stopping_return: int | None = None    # critical reward value for early stopping
     backup_depth: int = 100
     eval_interval: int = 2000                      # evaluate every N training steps
-    n_eval_episodes: int = 10                    # average eval rewards over N episodes
+    n_eval_episodes: int = 20                    # average eval rewards over N episodes
     num_training_steps: int = 3e5
- 
+    do_bootstrap: bool = True
+    do_baseline_sub: bool = True
+
+    def __post_init__(self) -> None:
+        if not (self.do_bootstrap or self.do_baseline_sub and self.agent_type == "actor_critic"):
+            raise UserWarning("Need at least either bootstrapping or baseline subtraction for actor-critic!!") 
